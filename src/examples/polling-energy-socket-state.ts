6544aeb79c98491cd08d581c479e8cb0366779cc
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2022-12-17 22:43:59 +0100

    chore: update docs and examples

--- src/examples/polling-energy-socket-state.ts
+++ src/examples/polling-energy-socket-state.ts
@@ -6,10 +6,17 @@ const api = new EnergySocketApi('http://192.168.1.32', {
   },
 });
 
-(async () => {
+(() => {
   api.polling.getState.start();
 
   api.polling.getState.on('response', response => {
+    // Receive the latest state of this Energy Socket from the /api/v1/state endpoint each second:
+    // {
+    //   power_on: false,
+    //   switch_lock: false,
+    //   brightness: 0
+    // }
+
     console.log('response', response);
   });
 
--- src/examples/polling-p1-meter-data.ts
+++ src/examples/polling-p1-meter-data.ts
@@ -10,6 +10,24 @@ const api = new P1MeterApi('http://192.168.1.35', {
   api.polling.getData.start();
 
   api.polling.getData.on('response', response => {
+    // Receive the latest data of this P1 Meter from the /api/v1/data endpoint each second:
+    // {
+    //   smr_version: 50,
+    //   meter_model: 'ISKRA 2M550T-1012',
+    //   wifi_ssid: 'SOME_WIFI_SSID',
+    //   wifi_strength: 100,
+    //   total_power_import_t1_kwh: 19055.287,
+    //   total_power_import_t2_kwh: 19505.815,
+    //   total_power_export_t1_kwh: 0.002,
+    //   total_power_export_t2_kwh: 0.007,
+    //   active_power_w: 997,
+    //   active_power_l1_w: 66,
+    //   active_power_l2_w: 88,
+    //   active_power_l3_w: 852,
+    //   total_gas_m3: 6789.488,
+    //   gas_timestamp: 221217223003
+    // }
+
     console.log('response', response);
   });
 
