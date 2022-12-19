8338d9a1b7df47d538d732aed18acb5f53a77558
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2022-12-19 07:19:15 +0100

    fix: expose polling methods for water meter properly

--- src/water-meter-api.ts
+++ src/water-meter-api.ts
@@ -1,5 +1,7 @@
-import { BaseApi, BaseApiOptions } from '@/base-api';
+import { BaseApi, BaseApiOptions, BasePolling } from '@/base-api';
 import { BasicInformationResponse, WaterMeterDataResponse } from '@/types';
+
+export type WaterMeterPolling = BasePolling<WaterMeterDataResponse>;
 
 export class WaterMeterApi extends BaseApi {
   getBasicInformation<T extends BasicInformationResponse>(): Promise<T> {
@@ -8,8 +10,18 @@ export class WaterMeterApi extends BaseApi {
   getData<T extends WaterMeterDataResponse>(): Promise<T> {
     return super.getData();
   }
+  startPolling(
+    method: 'getData',
+    apiMethod: <T extends WaterMeterDataResponse>() => Promise<T>,
+  ): Promise<void> {
+    return super.startPolling(method, apiMethod);
+  }
 
   constructor(baseUrl: string, options?: BaseApiOptions) {
     super(baseUrl, options);
+  }
+
+  get polling(): WaterMeterPolling {
+    return super.polling;
   }
 }
